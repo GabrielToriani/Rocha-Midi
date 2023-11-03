d88f74b304924a72902bf1bb1e2e35666fb28225
GabrielToriani <108074768+GabrielToriani@users.noreply.github.com>
2023-11-03 08:46:17 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-alert("Toriani")
+function tocaSom(seletorAudio) {
+    const eletento = Document. querySelector (seletorAudio);
+     if (elemento && elemento. localName === `audio´) {
+        elemento.play ();
+     }
+else{ 
+    alert//(elemento não encontrado´);
+    console. log (elemento não encontrado ou seletor inválido´)
+    
+}
